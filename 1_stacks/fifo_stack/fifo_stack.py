d439b3e428119aca881ee733fad46f01b6ad1619
Matthew Egan <matthewj.egan@hotmail.com>
2018-09-11 11:45:29 +1000

    add basic structure and fifo stack

---
+++ 1_stacks/fifo_stack/fifo_stack.py
@@ -0,0 +1,19 @@
+class FifoStack:
+    def __init__(self):
+        pass
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def __len__(self):
+        pass
+
