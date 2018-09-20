13e26dd4d579d13f650ccc9fd5191eece41db068
Matthew Egan <matthewj.egan@hotmail.com>
2018-09-20 20:52:30 +1000

    add solution for singularly linked list

--- data_structures/solutions/linked_lists/singularly_linked_list/singularly_linked_list.py
+++ data_structures/solutions/linked_lists/singularly_linked_list/singularly_linked_list.py
@@ -1,3 +1,67 @@
 
+class Node:
+    def __init__(self, next: "Node", value):
+        self.next = next
+        self.value = value
+
+
 class SingularlyLinkedList:
-    pass
+    def __init__(self):
+        self.head = Node(None, 1)
+    
+    def append(self, value):
+        curr = self.head
+        
+        while curr.next is not None:
+            curr = curr.next
+        
+        curr.next = Node(None, value)
+    
+    def insert(self, value, index):
+        curr = self.head
+        curr_index = 0
+
+        while curr_index < index - 1 and curr.next is not None:
+            curr = curr.next
+            curr_index += 1
+        
+        if curr_index != index - 1:
+            raise IndexError('Index not found in list')
+         
+        curr.next = Node(curr.next, value)
+    
+    def remove(self, index):
+        curr = self.head
+        curr_index = 0
+
+        while curr_index < index - 1 and curr.next is not None:
+            curr = curr.next
+            curr_index += 1
+        
+        if curr_index != index - 1:
+            raise IndexError('Index not found in list')
+         
+        curr.next = curr.next.next
+    
+    def get_index(self, index):
+        curr = self.head
+        curr_index = 0
+
+        while curr_index < index - 1 and curr.next is not None:
+            curr = curr.next
+            curr_index += 1
+        
+        if curr_index != index - 1:
+            raise IndexError('Index not found in list')
+
+        return curr.next.value
+
+    def __len__(self):
+        curr = self.head
+        count = 1
+
+        while curr.next is not None:
+            curr = curr.next
+            count += 1
+
+        return count
--- data_structures/tests/test_linked_lists/test_singularly_linked_list.py
+++ data_structures/tests/test_linked_lists/test_singularly_linked_list.py
@@ -3,4 +3,88 @@ from data_structures.linked_lists.singularly_linked_list.singularly_linked_list 
 from data_structures.solutions.linked_lists.singularly_linked_list.singularly_linked_list import SingularlyLinkedList as SingularlyLinkedListSolution
 
 class TestSingularlyLinkedList(TestCase):
-    pass
+    def test_append(self):
+        self._test_append(SingularlyLinkedList)
+
+    def test_solution_append(self):
+        self._test_append(SingularlyLinkedListSolution)
+    
+    def test_insert(self):
+        self._test_insert(SingularlyLinkedList)
+
+    def test_solution_insert(self):
+        self._test_insert(SingularlyLinkedListSolution)
+
+    def test_remove(self):
+        self._test_remove(SingularlyLinkedList)
+
+    def test_solution_remove(self):
+        self._test_remove(SingularlyLinkedListSolution)
+
+    def test_get_index(self):
+        self._test_get_index(SingularlyLinkedList)
+
+    def test_solution_get_index(self):
+        self._test_get_index(SingularlyLinkedListSolution)
+
+    def test_len(self):
+        self._test_len(SingularlyLinkedList)
+
+    def test_solution_len(self):
+        self._test_len(SingularlyLinkedListSolution)
+
+    def _test_append(self, SingularlyLinkedListKlass):
+        linked_list = SingularlyLinkedListKlass()
+        linked_list.append(2)
+        
+        assert linked_list.head.next is not None
+        assert linked_list.head.next.value == 2
+        assert linked_list.head.next.next is None
+    
+    def _test_insert(self, SingularlyLinkedListKlass):
+        linked_list = SingularlyLinkedListKlass()
+        linked_list.append(2)
+        linked_list.append(3)
+
+        linked_list.insert(4, 1)
+        linked_list.insert(5, 1)
+
+        assert linked_list.head.value == 1
+        assert linked_list.head.next.value == 5
+        assert linked_list.head.next.next.value == 4
+        assert linked_list.head.next.next.next.value == 2
+        assert linked_list.head.next.next.next.next.value == 3
+    
+    def _test_remove(self, SingularlyLinkedListKlass):
+        linked_list = SingularlyLinkedListKlass()
+        linked_list.append(2)
+        linked_list.append(3)
+
+        linked_list.remove(1)
+
+        assert linked_list.head.value == 1
+        assert linked_list.head.next.value == 3
+        assert linked_list.head.next.next is None
+
+        linked_list.remove(1)
+
+        assert linked_list.head.value == 1 
+        assert linked_list.head.next is None
+    
+    def _test_get_index(self, SingularlyLinkedListKlass):
+        linked_list = SingularlyLinkedListKlass()
+        linked_list.append(2)
+        linked_list.append(3)
+
+        assert linked_list.get_index(1) == 2
+        assert linked_list.get_index(2) == 3
+
+    def _test_len(self, SingularlyLinkedListKlass):
+        linked_list = SingularlyLinkedListKlass()
+        assert len(linked_list) == 1
+        
+        linked_list.append(2)
+        assert len(linked_list) == 2
+
+        linked_list.append(3)
+        assert len(linked_list) == 3
